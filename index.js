9700147a1c5a8f61cb64a6caea7c73bc3fab1ced
unknown <bassey.rhema@yahoo.com>
2023-08-19 18:00:47 +0100

    first commit

--- index.js
+++ index.js
@@ -2,8 +2,15 @@
 //only after the DOM is fully ready, providing a safe and reliable way to work with and manipulate elements on the page.
 document.addEventListener("DOMContentLoaded", function () {
   var button = document.getElementById("rollButton");
+  var imageContainer = document.getElementById('imageContainer');
+  var displayedImage = document.getElementById('displayedImage');
   button.addEventListener("click", function () {
+  
     dieFace = Math.floor(Math.random() * 6 + 1); //generates random numbers from [1,7), floor removes the decimal part, rounding to a lower number
-    alert("You rolled: " + dieFace);
+    
+    displayedImage.src = 'dice-face' + dieFace + '.png'
+    imageContainer.classList.remove('hidden');
   });
 });
+
+// image 4 & 5 are not to spec, (a bit smaller)
