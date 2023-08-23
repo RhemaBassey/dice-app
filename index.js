93eda13ad3fb41087af8bd4dc3510fe32628df8d
unknown <bassey.rhema@yahoo.com>
2023-08-23 18:20:12 +0100

    adding the ability to roll multiple dice

--- index.js
+++ index.js
@@ -4,19 +4,37 @@ document.addEventListener("DOMContentLoaded", function () {
   var button = document.getElementById("rollButton");
   var imageContainer = document.getElementById("imageContainer");
   var displayedImage = document.getElementById("displayedImage");
+
+  var dieCount = document.getElementById("dieCount");
+
   button.addEventListener("click", function () {
-    dieFace = Math.floor(Math.random() * 6 + 1); //generates random numbers from [1,7), floor removes the decimal part, rounding to a lower number
+    imageContainer.innerHTML = "";
+    console.log(dieCount.value);
+
+    // for (var i = 0; i < dieCount.value; i++) {
+    //   dieFace = Math.floor(Math.random() * 6 + 1); //generates random numbers from [1,7), floor removes the decimal part, rounding to a lower number
+    //   var newImage = document.createElement("img");
+    //   newImage.src = "images/dice-face" + dieFace + ".png";
+    //   newImage.alt = "dice " + dieFace;
+    //   imageContainer.appendChild(newImage);
+    // }
 
     imageContainer.classList.remove("hidden");
     imageContainer.classList.add("fade-in");
-    
+
     setTimeout(function () {
-      displayedImage.src = 'images/dice-face' + dieFace + '.png'
-    },500); //displayed image changes first, to avoid image jerking (where past image still appears for brief moment after transition)
+      for (var i = 0; i < dieCount.value; i++) {
+        dieFace = Math.floor(Math.random() * 6 + 1); //generates random numbers from [1,7), floor removes the decimal part, rounding to a lower number
+        var newImage = document.createElement("img");
+        newImage.src = "images/dice-face" + dieFace + ".png";
+        newImage.alt = "dice " + dieFace;
+        imageContainer.appendChild(newImage);
+      }
+  
+      // displayedImage.src = "images/dice-face" + dieFace + ".png";
+    }, 500); //displayed image changes first, to avoid image jerking (where past image still appears for brief moment after transition)
     setTimeout(function () {
       imageContainer.classList.remove("fade-in");
     }, 750); // a bit extra delay
-
   });
 });
-
